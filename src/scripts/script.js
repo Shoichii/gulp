f8087aace7d2afe5ecbe51915b61fbea2a835493
Shoichii <khayn1992@gmail.com>
2022-07-21 14:55:26 +0300

    assembly gulp

--- src/scripts/script.js
+++
@@ -1,6 +0,0 @@
-import {test23} from './test'
-
-const test = document.querySelector('.hiworld');
-test.style.backgroundColor = "blue";
-
-console.log(test23)
--- src/scripts/test.js
+++
@@ -1,1 +0,0 @@
-export const test23 = 'test';
